62abd510066c705a9cd938ce2f713b29662ccef4
Timur Aleshin <timur.aleshin@outlook.com>
2016-01-25 23:17:47 -0800

    Initial commit. At this point the bot will respond to 'bus' and give the next arrival for a hard coded stop number. It will then give updates every 5 seconds until it gets the 'nvm' command.

---
+++ bot.js
@@ -0,0 +1,60 @@
+var Botkit = require('botkit');
+var request = require('request');
+var fs = require('fs');
+var nconf = require('nconf');
+
+nconf.file({ file: './config.json'});
+
+var ONE_BUS_AWAY_KEY = nconf.get('ONE_BUS_AWAY');
+var SLACK_TOKEN = nconf.get('SLACK_TOKEN');
+var interval;
+
+var controller = Botkit.slackbot({
+ debug: true
+});
+
+controller.spawn({
+    token: SLACK_TOKEN
+}).startRTM(function(err) {
+    if (err) {
+        throw new Error(err);
+    }
+});
+
+controller.hears(['nvm'],['direct_message'],function(bot,message) {
+    if (interval) {
+        clearInterval(interval);
+    }
+});
+
+controller.hears(['bus'],['direct_message'],function(bot,message) {
+    bot.reply(message, 'Let me check...');
+    var oneBusAwayUrl = 'http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/' 
+        + '1_71334.json?key=' 
+        + ONE_BUS_AWAY_KEY;
+        
+    sayNextArrival(message, bot, oneBusAwayUrl);
+    interval = setInterval(sayNextArrival.bind(this, message, bot, oneBusAwayUrl), 5000);
+});
+
+sayNextArrival = function(message, bot, url) {
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var stopInfo = JSON.parse(body);
+            var nextBus = stopInfo.data.entry.arrivalsAndDepartures[0];
+            if (nextBus) {
+                var nextArrivalTime = new Date(nextBus.predictedArrivalTime);
+                bot.say({ 
+                    text: nextBus.routeShortName + ': ' + nextArrivalTime.toTimeString(),
+                    channel: message.channel
+                });    
+            } else {
+                bot.say({ 
+                    text: 'No arrivals in the next 35 min',
+                    channel: message.channel
+                });
+            }
+            
+        }
+    });
+}
