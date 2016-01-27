25b4db3974ddffa0960ed2b1f2d822148970ec90
Timur Aleshin <timur.aleshin@outlook.com>
2016-01-26 23:46:50 -0800

    Added a gulp task that creates a deploy folder which holds all the files that need to be published to Azure.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp'),
   ts = require('gulp-typescript'),
-  tslint = require('gulp-tslint');
+  tslint = require('gulp-tslint'),
+  replace = require('gulp-replace'),
+  rename = require('gulp-rename');
 
 var allTypescript = './bot-ts/**/*.ts'; 
 var allTypings = './typings/**/*.ts';
 var jsOutput = './bot-js/';
+var deployOutput = './deploy/';
 
 gulp.task('ts-lint', function () {
   return gulp.src(allTypescript).pipe(tslint()).pipe(tslint.report('prose'));
@@ -17,6 +20,21 @@ gulp.task('ts-compile', function() {
 
 gulp.task('watch', function() {
     gulp.watch([allTypescript], ['ts-lint', 'ts-compile']);
+});
+
+gulp.task('deploy', ['deploy-compile', 'deploy-move']);
+
+gulp.task('deploy-compile', function() {
+    return gulp.src([allTypescript, allTypings])
+    .pipe(ts({module: 'commonjs'})).js
+    .pipe(rename('app.js'))
+    .pipe(replace('nconf.get(\'ONE_BUS_AWAY\')', 'process.env.oneBusAway'))
+    .pipe(replace('nconf.get(\'SLACK_TOKEN\')', 'process.env.slackToken'))
+    .pipe(gulp.dest(deployOutput));
+});
+
+gulp.task('deploy-move', function() {
+    return gulp.src('./package.json').pipe(gulp.dest(deployOutput));
 });
 
 gulp.task('default', ['ts-lint', 'ts-compile', 'watch']);
